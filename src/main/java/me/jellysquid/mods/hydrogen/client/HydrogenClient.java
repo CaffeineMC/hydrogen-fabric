718f5032814c61f3746cc2300ae291f2cd2945f5
jellysquid3 <jellysquid3@users.noreply.github.com>
2021-01-23 21:10:22 -0600

    fix: Do not access ResourceType in pre-launch

--- src/main/java/me/jellysquid/mods/hydrogen/client/HydrogenClient.java
+++ src/main/java/me/jellysquid/mods/hydrogen/client/HydrogenClient.java
@@ -1,11 +1,13 @@
 package me.jellysquid.mods.hydrogen.client;
 
 import me.jellysquid.mods.hydrogen.client.resource.ModelCacheReloadListener;
+import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
 import net.minecraft.resource.ResourceType;
 
-public class HydrogenClientPreLaunch {
-    public static void init() {
+public class HydrogenClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
         ResourceManagerHelper.get(ResourceType.CLIENT_RESOURCES)
                 .registerReloadListener(new ModelCacheReloadListener());
     }
--- src/main/java/me/jellysquid/mods/hydrogen/common/HydrogenPreLaunch.java
+++ src/main/java/me/jellysquid/mods/hydrogen/common/HydrogenPreLaunch.java
@@ -1,18 +1,11 @@
 package me.jellysquid.mods.hydrogen.common;
 
-import me.jellysquid.mods.hydrogen.client.HydrogenClientPreLaunch;
 import me.jellysquid.mods.hydrogen.common.jvm.ClassConstructors;
-import net.fabricmc.api.EnvType;
-import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.loader.api.entrypoint.PreLaunchEntrypoint;
 
 public class HydrogenPreLaunch implements PreLaunchEntrypoint {
     @Override
     public void onPreLaunch() {
         ClassConstructors.init();
-
-        if (FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT) {
-            HydrogenClientPreLaunch.init();
-        }
     }
 }
