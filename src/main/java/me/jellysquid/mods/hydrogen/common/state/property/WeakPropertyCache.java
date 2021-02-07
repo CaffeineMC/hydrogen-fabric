d1b6971cd87cb2dfafb7745b9385bbc7112e159a
Mitchell Skaggs <skaggsm333@gmail.com>
2021-02-07 00:18:21 -0600

    Document WeakPropertyCache

--- src/main/java/me/jellysquid/mods/hydrogen/common/state/property/WeakPropertyCache.java
+++ src/main/java/me/jellysquid/mods/hydrogen/common/state/property/WeakPropertyCache.java
@@ -7,13 +7,23 @@ import java.util.WeakHashMap;
 
 public class WeakPropertyCache {
 
+    /**
+     * Values are type Void to prevent anything other than {@code null} from being in them since it's being used as a {@link java.util.Set}.
+     */
     private static final WeakHashMap<Property<?>, Void> CACHE = new WeakHashMap<>();
 
+    /**
+     * Caches properties passed into it in a {@link WeakHashMap}.
+     *
+     * @param newProperty the property to cache
+     * @param <T>         the property type (checked by the property's equals method)
+     * @return the cached property
+     */
+    @SuppressWarnings("unchecked")
     public static <T extends Property<?>> T tryCacheProperty(T newProperty) {
         // synchronized to prevent interleaving of separated contains-put pattern
         synchronized (CACHE) {
             // Checked by the property's equals method in MapKeyGetter#equals
-            @SuppressWarnings("unchecked")
             T originalKey = (T) MapKeyGetter.getOriginalKey(CACHE, newProperty);
             if (originalKey != null) {
                 System.err.println("Got \"" + originalKey + "\" from cache!");
