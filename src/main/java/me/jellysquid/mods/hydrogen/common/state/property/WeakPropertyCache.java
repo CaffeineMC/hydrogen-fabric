a339aeec4959c3cecc19ac9e994e4bf1b4edc3a8
Mitchell Skaggs <skaggsm333@gmail.com>
2021-02-11 13:30:24 -0600

    Use a WeakReference as the cache map value instead of using MapKeyGetter
    Co-authored-by: 2No2Name <2no2name@web.de>

--- src/main/java/me/jellysquid/mods/hydrogen/common/state/property/WeakPropertyCache.java
+++ src/main/java/me/jellysquid/mods/hydrogen/common/state/property/WeakPropertyCache.java
@@ -1,16 +1,16 @@
 package me.jellysquid.mods.hydrogen.common.state.property;
 
-import me.jellysquid.mods.hydrogen.common.collections.MapKeyGetter;
 import net.minecraft.state.property.Property;
 
+import java.lang.ref.WeakReference;
 import java.util.WeakHashMap;
 
 public class WeakPropertyCache {
 
     /**
-     * Values are type Void to prevent anything other than {@code null} from being in them since it's being used as a {@link java.util.Set}.
+     * Values are {@link WeakReference} because values in {@link WeakHashMap} are strongly-referenced by default.
      */
-    private static final WeakHashMap<Property<?>, Void> CACHE = new WeakHashMap<>();
+    private static final WeakHashMap<Property<?>, WeakReference<Property<?>>> CACHE = new WeakHashMap<>();
 
     /**
      * Caches properties passed into it in a {@link WeakHashMap}.
@@ -21,16 +21,6 @@ public class WeakPropertyCache {
      */
     @SuppressWarnings("unchecked")
     public static <T extends Property<?>> T tryCacheProperty(T newProperty) {
-        // synchronized to prevent interleaving of separated contains-put pattern
-        synchronized (CACHE) {
-            // Checked by the property's equals method in MapKeyGetter#equals
-            T originalKey = (T) MapKeyGetter.getOriginalKey(CACHE, newProperty);
-            if (originalKey != null) {
-                return originalKey;
-            } else {
-                CACHE.put(newProperty, null);
-                return newProperty;
-            }
-        }
+        return (T) CACHE.computeIfAbsent(newProperty, WeakReference::new).get();
     }
 }
