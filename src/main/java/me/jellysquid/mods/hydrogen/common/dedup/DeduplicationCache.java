905e7b0eb585f4a71a16058bfe4c9004b700f9b4
jellysquid3 <jellysquid3@users.noreply.github.com>
2021-06-18 17:17:21 -0500

    change: Synchronize de-duplication cache

--- src/main/java/me/jellysquid/mods/hydrogen/common/dedup/DeduplicationCache.java
+++ src/main/java/me/jellysquid/mods/hydrogen/common/dedup/DeduplicationCache.java
@@ -34,21 +34,22 @@ public class DeduplicationCache<T> {
         return this.cache.addOrGet(item);
     }
 
-    public void clearCache() {
-        this.cache.clear();
+    public synchronized void clearCache() {
         this.attemptedInsertions = 0;
+
+        this.cache.clear();
     }
 
-    public int getSize() {
+    public synchronized int getSize() {
         return this.cache.size();
     }
 
-    public int getDeduplicatedCount() {
+    public synchronized int getDeduplicatedCount() {
         return this.attemptedInsertions - this.cache.size();
     }
 
     @Override
-    public String toString() {
+    public synchronized String toString() {
         return String.format("DeduplicationCache ( %d de-duplicated, %d entries )",
                 this.getDeduplicatedCount(), this.getSize());
     }
