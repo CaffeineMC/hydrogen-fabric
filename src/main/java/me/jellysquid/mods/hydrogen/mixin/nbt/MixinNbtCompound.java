43c79fc7dfcecf92317091c20ffcf10d708026a0
jellysquid3 <jellysquid3@users.noreply.github.com>
2021-06-18 16:08:30 -0500

    refactor: Adjust Mixin names following Yarn changes

--- src/main/java/me/jellysquid/mods/hydrogen/mixin/nbt/MixinNbtCompound.java
+++ src/main/java/me/jellysquid/mods/hydrogen/mixin/nbt/MixinNbtCompound.java
@@ -15,7 +15,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import java.util.Map;
 
 @Mixin(NbtCompound.class)
-public class MixinCompoundTag {
+public class MixinNbtCompound {
     @Mutable
     @Shadow
     @Final
